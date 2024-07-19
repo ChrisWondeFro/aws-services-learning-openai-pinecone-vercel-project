2cd9cf7c069c2fe35fb137c25b7dab1e22dd8edb
Chrismanati <christianwondirad1213@gmail.com>
2024-07-19 02:42:28 -0600

    added more AWS services urls to crawl and index..

--- src/app/components/Context/urls.ts
+++ src/app/components/Context/urls.ts
@@ -1,5 +1,41 @@
 export const urls = [
 {
+  url: "https://docs.aws.amazon.com/whitepapers/latest/aws-overview/compute-services.html",
+  title: "AWS Compute services",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://docs.aws.amazon.com/whitepapers/latest/aws-overview/analytics.html",
+  title: "AWS Analytics",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://docs.aws.amazon.com/whitepapers/latest/aws-overview/networking-services.html",
+  title: "AWS Networking and content delivery",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://docs.aws.amazon.com/whitepapers/latest/aws-overview/security-services.html",
+  title: "AWS Security, identity, and compliance",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://docs.aws.amazon.com/whitepapers/latest/aws-overview/management-governance.html",
+  title: "AWS Management and governance",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://docs.aws.amazon.com/whitepapers/latest/building-scalable-secure-multi-vpc-network-infrastructure/welcome.html?did=wp_card&trk=wp_card",
+  title: "Building a Scalable and Secure Multi-VPC AWS Network Infrastructure",
+  seeded: false,
+  loading: false,
+},
+{
   url: "https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/concepts.html",
   title: "What is Amazon EC2",
   seeded: false,
